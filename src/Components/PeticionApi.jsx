efece694b612ea6dbdb0b8a2fa2e98db6f7de38d
unknown <geralgranadosb@gmail.com>
2023-05-26 20:30:28 -0500

    Tercer commit

--- src/Components/PeticionApi.jsx
+++ src/Components/PeticionApi.jsx
@@ -23,8 +23,24 @@ const atrás = () =>{
   listadoNaves(paginacion-2)
 }
   return (
+    <div className="body">
+      <h1 > PETICIÓN DE NAVES ESPACIALES</h1><hr/>
+      <th><button onClick={listadoNaves}>Ver Naves Espaciales</button> </th>
+      <th><button onClick={siguiente}>Siguiente</button></th>
+      <th><button onClick={atrás}>Atrás</button><br /></th>
+      <hr />
     
-
+      {
+        naves.map(({rocket_name: name,flickr_images: image})=> (
+          <div className="contenedor">
+          <h4>{name}</h4>
+          <img src={image} alt="" />
+          </div>
+        )
+        )
+       
+      }
+</div>
   )
 }
 
